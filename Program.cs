8aacb33672a650ca50a505ed0ba711721df743f0
breez93 <48477753+breez93@users.noreply.github.com>
2019-10-03 18:28:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DiceGame
+{
+    
+        class Program
+        {
+
+
+            public static void Main()
+            {
+                bool active = true;
+                Console.WriteLine("DiceGame || Console Version");
+                Console.WriteLine("Created by Paulo Rafael");
+                Console.WriteLine("-----------------------------------");
+                while (active)
+                {
+
+                    Console.Write("--- Commands: \n");
+                    Console.WriteLine("-- Roll\n" +
+                                      "-- Clear\n" +
+                                      "-- Exit\n");
+                    string command = Console.ReadLine();
+                    switch (command)
+                    {
+                        case "Roll":
+                            Console.Clear();
+                            Console.WriteLine("\n\n\n\n");
+                            Roll();
+                            Console.WriteLine("\n\n\n\n");
+                            break;
+                        case"exit":
+                            active = false;
+                            break;
+                        case "clear":
+                            Console.Clear();
+                            break;
+                    }
+                }
+            }
+
+            public static void Roll()
+            {
+
+                Random random = new Random();
+                int Dice1 = random.Next(1, 6);
+                int Dice2 = random.Next(1, 6);
+                Console.WriteLine("  --Your value was: " + Dice1 + "  --My value was: " + Dice2);
+                Main();
+            }
+
+
+
+        }
+    }
+
+
